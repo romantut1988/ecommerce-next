1f9d15c312037dac8c5f37bbb44cdba582f13609
Roman <romantut1988@mail.ru>
2023-11-17 14:52:12 +0300

    start

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "D:\\ecommerce-next\\app\\layout.tsx": [
+      "D:\\ecommerce-next\\styles\\globals.css",
+      "D:\\ecommerce-next\\node_modules\\next\\font\\google\\target.css?{\"path\":\"lib\\\\fonts.ts\",\"import\":\"Plus_Jakarta_Sans\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-sans\"}],\"variableName\":\"fontSans\"}",
+      "D:\\ecommerce-next\\node_modules\\next\\font\\google\\target.css?{\"path\":\"lib\\\\fonts.ts\",\"import\":\"JetBrains_Mono\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-mono\"}],\"variableName\":\"fontMono\"}"
+    ]
+  },
+  "cssModules": {
+    "D:\\ecommerce-next\\app\\page": [
+      "D:\\ecommerce-next\\styles\\globals.css",
+      "D:\\ecommerce-next\\node_modules\\next\\font\\google\\target.css?{\"path\":\"lib\\\\fonts.ts\",\"import\":\"Plus_Jakarta_Sans\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-sans\"}],\"variableName\":\"fontSans\"}",
+      "D:\\ecommerce-next\\node_modules\\next\\font\\google\\target.css?{\"path\":\"lib\\\\fonts.ts\",\"import\":\"JetBrains_Mono\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-mono\"}],\"variableName\":\"fontMono\"}"
+    ]
+  }
+}
